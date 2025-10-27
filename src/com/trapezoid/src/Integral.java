a300a27c06d322d098228d5e8550698689f2f55c
charisma tubagus <charisma.tubagus@gmail.com>
2025-10-27 14:27:37 +0700

    Added implementation Trapezoid

--- src/com/trapezoid/src/Integral.java
+++ src/com/trapezoid/src/Integral.java
@@ -2,23 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.riemansums.src;
+package com.trapezoid.src;
 
 /**
  *
  * @author Administrator
  */
 public class Integral {
-    static double fx(double x){
+     static double fx(double x){
         return x*x;
     }
     static double integral(int bb, int ba){
         double deltax = (ba-bb)/100000.0f;
         double sum = 0;
         for(double i = bb ; i <= ba; i+= deltax){
-            sum += fx(i) * deltax;
+            if( i == bb || i == ba){
+                sum += fx(i);
+            }else {
+                sum += 2*fx(i);
+            }
+            
         }
-        return sum;
+        return sum*deltax/2;
     }
     public static void main(String[] args) {
         double hasil = integral(1,3);

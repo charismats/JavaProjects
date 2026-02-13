783ecfd653350680511f14fabe8f2285b5cabf5a
charismatubagus <charisma.tubagus@gmail.com>
2026-02-14 05:18:32 +0700

    Added SOCS 5

---
+++ src/com/socs5/src/ProblemA.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.socs5.src;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author chari
+ */
+public class ProblemA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0 ; i < T ; i++){
+            double percent = sc.nextDouble();
+            double value = sc.nextDouble();
+            double res = percent * value / 100;
+            System.out.println(res);
+        }
+    }
+}
